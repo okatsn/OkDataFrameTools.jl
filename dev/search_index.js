af36de14548da5f3c0c0ebec01f60ed17e8800ba
Documenter.jl <documenter@juliadocs.github.io>
2023-04-19 08:18:40 +0000

    build based on 2bf61fb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OkDataFrameTools","category":"page"},{"location":"#OkDataFrameTools","page":"Home","title":"OkDataFrameTools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkDataFrameTools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OkDataFrameTools]","category":"page"}]
+}
