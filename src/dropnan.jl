5ff702b57562771320349b8fa64af1b1c78eeb39
okatsn <okatsn@gmail.com>
2023-11-16 17:25:51 +0800

    v0.2.0 better dropnanmissing!

--- src/dropnan.jl
+++ src/dropnan.jl
@@ -1,8 +1,23 @@
-function dropnanmissing!(dfcb)
-    allowmissing!(dfcb)
-    for col in eachcol(dfcb)
-        replace!(col, NaN => missing)
-    end
-    dropmissing!(dfcb) # if there is NaN, stacked plot goes wrong
+"""
+`dropnanmissing` replaces `NaN` by `missing`, `dropmissing!` and `disallowmissing`.
+"""
+function dropnanmissing!(dfcb; kwargs...)
+    _preprocess!(dfcb, All())
+    dropmissing!(dfcb; kwargs...) # if there is NaN, stacked plot goes wrong
     disallowmissing!(dfcb)
+end
+
+"""
+"""
+function dropnanmissing!(dfcb, cols; kwargs...)
+    _preprocess!(dfcb, cols)
+    dropmissing!(dfcb, cols; kwargs...) # if there is NaN, stacked plot goes wrong
+end
+
+function _preprocess!(dfcb, cols)
+    colnames = names(dfcb, cols)
+    allowmissing!(dfcb)
+    for col in colnames
+        replace!(dfcb[!, col], NaN => missing)
+    end
 end
--- test/dropnan.jl
+++ test/dropnan.jl
@@ -4,20 +4,26 @@
     v[2] = NaN
     v[3] = NaN
     df1 = DataFrame(
-        :a => [1,   2,    3,     1,    2],
+        :a => [1, 2, 3, 1, 2],
         :b => v, #  NaN,  NaN
         :c => ["A", "A", missing, "B", missing],
     )
     df2 = DataFrame(
-        :a => [1,   2,    3,     1,    2],
+        :a => [1, 2, 3, 1, 2],
         :b => v, #  NaN,  NaN
         :c => ["A", "A", missing, "B", NaN],
     )
 
+    df3 = deepcopy(df2)
+
     dropnanmissing!(df1)
     dropnanmissing!(df2)
+    dropnanmissing!(df3, [:c])
     @test df1 !== df2
     @test nrow(df1) == 2
     @test nrow(df2) == 2
     @test df1 == df2
+    @test isnan(df3.b[2]) # shouldn't be replaced by missing
+    @test isequal(df3.c, ["A", "A", "B"])
+    @test isequal(df3.a, [1, 2, 1])
 end
